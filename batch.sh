f1ef8c8c2e9e9bc2d66bcdd4226775ba46456e7b
guoyilqbjm <2268323136@qq.com>
2016-05-13 13:43:32 +0800

    check batch.sh

--- batch.sh
+++ batch.sh
@@ -2,3 +2,16 @@ cd Code
 make clean
 cd ..
 git status
+git add -A
+echo "Finished adding all changes into your local git."
+echo -n "Please input the summary for this submit:"
+read summary
+if [$summary = "\n"]
+then
+	echo "Because of empty input for submit summary, don't finish push work."
+else
+	git commit -m "$summary"
+	git push "http://github.com/gybjm2016/Compiler.git" master
+fi
+
+
