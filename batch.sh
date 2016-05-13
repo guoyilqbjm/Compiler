9861121af9d646478a4957aec35742913d35babd
gybjm2016 <2268323136@qq.com>
2016-05-13 18:08:57 +0800

    change batch.sh file.

--- batch.sh
+++ batch.sh
@@ -1,17 +1,47 @@
-cd Code
-make clean
-cd ..
-git status
-git add -A
-echo "Finished adding all changes into your local git."
-echo -n "Please input the summary for this submit:"
-read summary
-if [$summary = "\n"]
-then
-	echo "Because of empty input for submit summary, don't finish push work."
-else
-	git commit -m "$summary"
-	git push "http://github.com/gybjm2016/Compiler.git" master
+#!/bin/sh
+#convient for compiler expriment.
+
+github='http://www.github.com/Compiler.git'
+
+if [ ! -x batch.sh ]; then
+	chmod +x "batch.sh"
 fi
 
+if [ $# -lt 1 ]; then
+	cat<<HELP
+Illegal Use.
+usage: sh batch.sh [ARGUMENTS]
+   or: ./batch.sh [ARGUMENTS]
+ARGUMENTS is one type of the followings:
+    pull      pull all dates from "${github}" into master.
+    commit    commit all changes to local git.
+    submit    submit all local git to "${github}" by your account.
+HELP
+	exit 0
+fi			
 
+SWPFILE=find ./ -name "*.swp"
+if [ $1 = "commit" ]; then
+	cd Code
+	make clean
+	cd ..
+	rm -f $SWPFILE
+	git add -A
+	git commit 
+elif [ $1 = "submit" ]; then
+	git push ${github} master
+else
+	if [ $1 = "pull" ]; then
+		git pull ${github} master
+	else
+	  cat<<HELP
+Illegal Use.
+usage: sh batch.sh [ARGUMENTS]
+   or: ./batch.sh [ARGUMENTS]
+ARGUMENTS is one type of the followings:
+    pull      pull all dates from "${github}" into master.
+    commit    commit all changes to local git.
+    submit    submit all local git to "${github}" by your account.
+HELP
+	fi
+fi
